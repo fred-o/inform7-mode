4972cbd7bc8e7d7a7455525976b0cff8d0a6e856
fred-o <fredrik.appelberg@gmail.com>
2012-02-29 17:03:18 +0100

    hightling and indenting

--- inform7-mode.el
+++ inform7-mode.el
@@ -1,3 +1,11 @@
+(defgroup inform7 nil
+  "Customizations for inform7-mode."
+  :group 'languages)
+
+(defface inform7-heading-face
+  '((t (:inherit font-lock-preprocessor-face :weight bold :height 1.2)))
+  "Face for Inform 7 headings"
+  :group 'font-lock-highlighting-faces)
 
 (defvar inform7-mode-map
   (let ((map (make-sparse-keymap)))
@@ -9,12 +17,12 @@
 (add-to-list 'auto-mode-alist '("\\.ni" . inform7-mode))
 
 (defconst inform7-font-lock-keywords
-  `(( ,(regexp-opt '("Part" "Chapter.*" "Section") nil) . font-lock-constant-face)
-    ( ,(regexp-opt '("Include" "Use" "let" "say" "if" "otherwise") 'words) . font-lock-keyword-face)
-    ("\\(\\[.*?\\]\\)" 0 font-lock-keyword-face t))
+  `(( ,(regexp-opt '("Include" "Use" "let" "say" "if" "otherwise") 'words) . font-lock-keyword-face)
+    ("\\(\\(?:Book\\|Chapter\\|Part\\|Section\\|Volume\\) - .*\\)" 0 'inform7-heading-face t)
+    ("\\(\\[.*?\\]\\)" 0 font-lock-variable-name-face t))
   "Highlighting expressions for inform7-mode")
 
-(define-derived-mode inform7-mode fundamental-mode
+(define-derived-mode inform7-mode sws-mode
   "Inform7"
   "Major mode for editing inform 7 story files."
   (interactive)
@@ -22,6 +30,15 @@
 
   (setq mode-name "Inform7")
   (setq major-mode 'inform7-mode)
+
+  ;; default tab width
+  (setq sws-tab-width 2)
+  (make-local-variable 'indent-line-function)
+  (setq indent-line-function 'sws-indent-line)
+  (make-local-variable 'indent-region-function)
+
+  (setq indent-region-function 'sws-indent-region)
+
   (use-local-map inform7-mode-map)
   (set (make-local-variable 'font-lock-defaults) '(inform7-font-lock-keywords nil t)))
 
