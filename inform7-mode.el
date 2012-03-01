4ef29210ca74909bd9ed1cd383c0b86ff1bef1df
fred-o <fredrik.appelberg@gmail.com>
2012-03-01 09:04:28 +0100

    stripping out unimportant stuff

--- inform7-mode.el
+++ inform7-mode.el
@@ -1,7 +1,3 @@
-(defgroup inform7 nil
-  "Customizations for inform7-mode."
-  :group 'languages)
-
 (defface inform7-heading-face
   '((t (:inherit font-lock-preprocessor-face :weight bold :height 1.2)))
   "Face for Inform 7 headings"
@@ -14,33 +10,17 @@
     map)
   "Keymap for inform7 major mode")
 
-;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.ni" . inform7-mode))
-
 (defconst inform7-font-lock-keywords
   `(( ,(regexp-opt '("Include" "Use" "let" "say" "if" "otherwise") 'words) . font-lock-keyword-face)
+    ("\\[.*\\]" . font-lock-comment-face)
     ("\\(\\(?:Book\\|Chapter\\|Part\\|Section\\|Volume\\) - .*\\)" 0 'inform7-heading-face t)
-    ("\\(\\[.*?\\]\\)" 0 font-lock-variable-name-face t))
+    ("\".*\\(\\[.*?\\]\\).*\"" 1 font-lock-variable-name-face t))
   "Highlighting expressions for inform7-mode")
 
 (define-derived-mode inform7-mode sws-mode
   "Inform7"
   "Major mode for editing inform 7 story files."
-  (interactive)
-  (kill-all-local-variables)
-
-  (setq mode-name "Inform7")
-  (setq major-mode 'inform7-mode)
-
-  ;; default tab width
-  (setq sws-tab-width 2)
-  (make-local-variable 'indent-line-function)
-  (setq indent-line-function 'sws-indent-line)
-  (make-local-variable 'indent-region-function)
-
-  (setq indent-region-function 'sws-indent-region)
-
-  (use-local-map inform7-mode-map)
+  (visual-line-mode)
   (set (make-local-variable 'font-lock-defaults) '(inform7-font-lock-keywords nil t)))
 
 (provide 'inform7-mode)
